6ac0cd850924ad42ef8175f35564ac3c6af5bcaf
Taikimen52 <5taikiiida@gmail.com>
2025-10-19 23:07:10 +0900

    Struggling with delete button function.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// 必要な定数
+// グローバル定数・変数
 const myLibrary = [];
 const btn = document.querySelector("#datasubmit");
 const form = document.querySelector("#inputform");
+const deleteBtn = document.querySelectorAll(".deletebtn");
 
-
+// コンストラクタ・オブジェクト
 function Book(){
 }
 
-//form入力された情報を元にインスタンスを生成し配列に追加
+// 関数（1関数1タスク）
+//form入力された情報を元にBookインスタンスを生成し配列に追加
 function addBookToLibrary(name, author, page, price) {
     const currentBook = new Book;
     currentBook.id = crypto.randomUUID();
@@ -32,13 +34,30 @@ function displayBooks(){
                 <p>Total Page : ${book.page}</p>
                 <p>Price : ¥${book.price}</p>
             </div>
+            <div class="delete">
+                <button class="deletebtn" id="${book.id}">Delete this book</button>
+            </div>
         </div>`
         contents.innerHTML += bookInfo;
+
+        // ここでdeleteBtnにイベントリスナーをくっつける。
+        const deleteBtn = document.querySelector(`#${book.id}`);
+        deleteBtn.addEventListener("click", deleteBook(book.id));
     }
 }
 
-btn.addEventListener("click",(e)=>{
-    e.preventDefault;
+// ライブラリから特定のBookを削除する
+function deleteBook(targetId) {
+    const taregetIndex = myLibrary.findIndex((book)=>{book.id === targetId});
+    if(taregetIndex != -1){
+        myLibrary.splice(taregetIndex, 1);
+    }
+}
+
+
+// イベントリスナー系
+btn.addEventListener("click",(event)=>{
+    event.preventDefault;
     const bookName = document.querySelector("#bookname").value;
     const author = document.querySelector("#author").value;
     const page = document.querySelector("#page").valueAsNumber;
@@ -48,5 +67,16 @@ btn.addEventListener("click",(e)=>{
     form.reset();
     displayBooks()
 } )
+
+// 各BookカードのDeleteボタンに機能を実装
+// deleteBtn.forEach(el => {
+//     el.addEventListener("click", (event)=> {
+//         // idを取得
+//         const targetId = event.currentTarget.getAttribute("id");
+//         console.log(targetId)
+//         deleteBook(targetId);
+//         console.log(myLibrary);
+//     })
+// })
 
 
